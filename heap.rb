e1bbae033534a29c24de67957eb26d1d9841c7f2
Bryan <cbfowler4@gmail.com>
2018-03-04 12:55:28 -0500

    basic class structure created

---
+++ heap.rb
@@ -0,0 +1,60 @@
+
+
+class MinHeap
+  include HeapUtil
+
+  def initialize()
+    @heap = [];
+  end
+
+  def removeMin(index)
+  end
+
+  def add(el)
+  end
+
+  def swap(from, to)
+  end
+
+  def heapifyUp()
+  end
+
+  def heapifyDown()
+  end
+
+end
+
+
+
+
+module HeapUtil
+  def getLeftChildIndex(index)
+  end
+
+  def getRightChildIndex(index)
+  end
+
+  def getParentIndex(index)
+  end
+
+  def hasLeftChild?(index)
+  end
+
+  def hasRightChild?(index)
+  end
+
+  def hasParent?(index)
+  end
+
+  def leftChild(index)
+  end
+
+  def rightChild(index)
+  end
+
+  def parent(index)
+  end
+
+  def peek()
+  end
+end
