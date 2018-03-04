2c9eea8121f4d237f60ffe740d7c5fcba785baac
Bryan <cbfowler4@gmail.com>
2018-03-04 13:06:48 -0500

    heap util module built out

--- heap.rb
+++ heap.rb
@@ -13,6 +13,9 @@ class MinHeap
   def add(el)
   end
 
+
+  private
+  
   def swap(from, to)
   end
 
@@ -29,32 +32,42 @@ end
 
 module HeapUtil
   def getLeftChildIndex(index)
+    2*index+1
   end
 
   def getRightChildIndex(index)
+    2*index+2
   end
 
   def getParentIndex(index)
+    (index-1)/2
   end
 
   def hasLeftChild?(index)
+    getLeftChildIndex(index) > @heap.length - 1
   end
 
   def hasRightChild?(index)
+    getRightChildIndex(index) > @heap.length - 1
   end
 
   def hasParent?(index)
+    index != 0
   end
 
   def leftChild(index)
+    hasLeftChild?(index) ? @heap[getLeftChildIndex(index)] : false
   end
 
   def rightChild(index)
+    hasRightChild?(index) ? @heap[getRightChildIndex(index)] : false
   end
 
   def parent(index)
+    hasParent?(index) ? @heap[getParentIndex(index)] : false
   end
 
-  def peek()
+  def peek
+    @heap[0]
   end
 end
