37f8752f8ac0afb82e5f848c879174598b5fa222
Bryan <cbfowler4@gmail.com>
2018-03-07 18:30:57 -0500

    quicksort completed

--- sorting.js
+++ sorting.js
@@ -30,4 +30,27 @@ function merge(left, right) {
   return sortedArray.concat(left).concat(right);
 }
 
-module.exports = { 'mergeSort': mergeSort };
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const
+    pivot = array[0],
+    left = [],
+    right = [];
+
+  array.slice(1).forEach((el) => {
+    if (el > pivot) {
+      right.push(el);
+    } else {
+      left.push(el);
+    }
+  });
+
+  return quickSort(left).concat([pivot]).concat(quickSort(right));
+}
+
+module.exports = {
+  'mergeSort': mergeSort,
+  'quickSort': quickSort};
