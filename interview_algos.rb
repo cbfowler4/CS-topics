3836fb1e8439c050806e855f9652c4e9f1fbb1cb
Bryan <cbfowler4@gmail.com>
2018-03-09 16:00:36 -0500

    window solution started

--- interview_algos.rb
+++ interview_algos.rb
@@ -3,29 +3,45 @@
 # output: aaccaaaa
 
 
-def longestSub(string)
-  #store character and pointer in array
-  set = [[], []];
-  longStart = 0;
-  longest = "";
+# def longestSub(string)
+#   #store character and pointer in array
+#   set = [[], []];
+#   longStart = 0;
+#   longest = "";
+#
+#   string.each_with_index do |char, idx|
+#     if char != set[0][0] && char != set[1][0]
+#       #check to see if current set longer than longest
+#       if idx - longStart > longest.length
+#         longest = string[idx...longStart]
+#       end
+#
+#       #update set
+#       set.shift
+#       set.push([char, idx])
+#       longStart = set[0][1]
+#
+#     elsif char == set[1][0]
+#
+#
+#     end
+#
+#
+#   end
+# end
 
-  string.each_with_index do |char, idx|
-    if char != set[0][0] && char != set[1][0]
-      #check to see if current set longer than longest
-      if idx - longStart > longest.length
-        longest = string[idx...longStart]
-      end
-
-      #update set
-      set.shift
-      set.push([char, idx])
-      longStart = set[0][1]
-
-    elsif char == set[1][0] 
+def longestSub(str)
 
 
-    end
+end
 
+class Window
+  def initialize
+    @cont = []
+    @num_uniq_chars = 2
+  end
 
+  def isValid?
+    
   end
 end
